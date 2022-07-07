f3c657a87646a67701ec9609c4f5f262a88215e0
EdisonRUI <zrui0761@gmail.com>
2022-07-07 23:28:28 +0800

    #4 实现部分功能模块

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
-        cout << "Hello Demo" << endl;
-        return 0;
+    cout << "# Welcome to ByteYoung DB!!!" << endl;
+    cout << "# Input your query in one line." << endl;
+    cout << "# Enter 'exit' to quit this program." << endl;
+
+    string cmd;
+    while (true) {
+        std::getline(std::cin, cmd);
+        if (cmd == "exit") {
+            break;
+        }
+    }
+
+    cout << "# Farewell~~~ " << std::endl;
+    return 0;
 }
